28d3b1c652a166b1eb63496e52ebc78fb828c523
Joey“ <yuyong1982912@gmail.com>
2026-02-10 11:46:10 +0800

    Create new post

---
+++ Publish.bat
@@ -0,0 +1,29 @@
+﻿@echo off
+setlocal
+
+echo Step1
+call hexo clean
+if errorlevel 1 goto :fail
+
+echo Step2
+call hexo generate
+if errorlevel 1 goto :fail
+
+echo Step3
+git add .
+if errorlevel 1 goto :fail
+
+echo Step4
+git commit -m "Create new post"
+if errorlevel 1 goto :fail
+
+echo step5
+git push
+if errorlevel 1 goto :fail
+
+echo All done
+exit /b 0
+
+:fail
+echo Failed with error %errorlevel%
+exit /b %errorlevel%
